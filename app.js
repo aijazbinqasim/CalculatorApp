4d7a5ebf19b5b65d8ce9edb89d9a310ff817c42a
aijazbinqasim <aijazbinqasim@hotmail.com>
2021-08-17 15:52:10 +0500

    Add simple calculator App.

---
+++ app.js
@@ -0,0 +1,40 @@
+var v1 = 0, v2 = 0;
+
+function add() {
+    document.getElementById('operator').innerHTML = '<strong> + </strong>';
+    v1 = parseFloat(document.getElementById('v1').value);
+    v2 = parseFloat(document.getElementById('v2').value);
+    document.getElementById('ans').innerHTML = '<strong> = ' + (v1 + v2) + '</strong>';
+}
+
+function substract() {
+    document.getElementById('operator').innerHTML = '<strong> - </strong>';
+    v1 = parseFloat(document.getElementById('v1').value);
+    v2 = parseFloat(document.getElementById('v2').value);
+    document.getElementById('ans').innerHTML = '<strong> = ' + (v1 - v2) + '</strong>';
+}
+
+function multiply() {
+    document.getElementById('operator').innerHTML = '<strong> x </strong>';
+    v1 = parseFloat(document.getElementById('v1').value);
+    v2 = parseFloat(document.getElementById('v2').value);
+    document.getElementById('ans').innerHTML = '<strong> = ' + (v1 * v2) + '</strong>';
+}
+
+function divide() {
+    document.getElementById('operator').innerHTML = '<strong> &#247; </strong>';
+    v1 = parseFloat(document.getElementById('v1').value);
+    v2 = parseFloat(document.getElementById('v2').value);
+    document.getElementById('ans').innerHTML = '<strong> = ' + (v1 / v2) + '</strong>';
+}
+
+function c() {
+    v1 = document.getElementById('v1').value = '0';
+    v2 = document.getElementById('v2').value = '0';
+    document.getElementById('ans').innerHTML = null;
+    document.getElementById('operator').innerHTML = null;
+}
+
+function developer() {
+    alert('This App is developed by : Sajid Jamali');
+}
