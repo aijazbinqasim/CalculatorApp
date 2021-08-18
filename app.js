fc073f6187e1320965457c5263da3279964ae018
aijazbinqasim <aijazbinqasim@hotmail.com>
2021-08-18 12:43:22 +0500

    Improvement.

--- app.js
+++ app.js
@@ -4,39 +4,45 @@ function add() {
     document.getElementById('operator').innerHTML = '<strong> + </strong>';
     v1 = parseFloat(document.getElementById('v1').value);
     v2 = parseFloat(document.getElementById('v2').value);
-    document.getElementById('ans').innerHTML = '<strong> = ' + (v1 + v2) + '</strong>';
+    var total = v1 + v2;
+    document.getElementById('ans').innerHTML = '<strong> = ' + total + '</strong>';
+    document.getElementById('v1').value = total;
+    document.getElementById('v2').value = '0';
 }
 
 function substract() {
     document.getElementById('operator').innerHTML = '<strong> - </strong>';
     v1 = parseFloat(document.getElementById('v1').value);
     v2 = parseFloat(document.getElementById('v2').value);
-    document.getElementById('ans').innerHTML = '<strong> = ' + (v1 - v2) + '</strong>';
+    var total = v1 - v2;
+    document.getElementById('ans').innerHTML = '<strong> = ' + total + '</strong>';
+    document.getElementById('v1').value = total;
+    document.getElementById('v2').value = '0';
 }
 
 function multiply() {
     document.getElementById('operator').innerHTML = '<strong> x </strong>';
     v1 = parseFloat(document.getElementById('v1').value);
     v2 = parseFloat(document.getElementById('v2').value);
-    document.getElementById('ans').innerHTML = '<strong> = ' + (v1 * v2) + '</strong>';
+    var total = v1 * v2;
+    document.getElementById('ans').innerHTML = '<strong> = ' + total + '</strong>';
+    document.getElementById('v1').value = total;
+    document.getElementById('v2').value = '0';
 }
 
 function divide() {
     document.getElementById('operator').innerHTML = '<strong> &#247; </strong>';
     v1 = parseFloat(document.getElementById('v1').value);
     v2 = parseFloat(document.getElementById('v2').value);
-    document.getElementById('ans').innerHTML = '<strong> = ' + (v1 / v2) + '</strong>';
+    var total = v1 / v2;
+    document.getElementById('ans').innerHTML = '<strong> = ' + total + '</strong>';
+    document.getElementById('v1').value = total;
+    document.getElementById('v2').value = '0';
 }
 
 function c() {
-    if (confirm('Are you sure to Clear?')) {
-        v1 = document.getElementById('v1').value = '0';
-        v2 = document.getElementById('v2').value = '0';
-        document.getElementById('ans').innerHTML = null;
-        document.getElementById('operator').innerHTML = null;
-    }
-}
-
-function developer() {
-    alert('This App is developed by : Sajid Jamali');
+    v1 = document.getElementById('v1').value = '0';
+    v2 = document.getElementById('v2').value = '0';
+    document.getElementById('ans').innerHTML = null;
+    document.getElementById('operator').innerHTML = null;
 }
